6b3816a0a322e1d06d61d58bdf2da2ef227c1ea3
Billy Dorman <b.d.dorman12@gmail.com>
2021-12-03 13:33:37 -0600

    set background color, scale/position doll

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+//set background color
+renderer.setClearColor( 0xb7c3f3, 1 );
+
+// add ambient light
+const light = new THREE.AmbientLight( 0xffffff );
+scene.add( light );
+
 // const geometry = new THREE.BoxGeometry();
 // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 // const cube = new THREE.Mesh( geometry, material );
@@ -14,7 +21,15 @@ document.body.appendChild( renderer.domElement );
 camera.position.z = 5;
 
 // gltf loader for models
-const loader = new GLTFLoader();
+const loader = new THREE.GLTFLoader();
+
+// call loader function
+loader.load("../SGmodels/scene.gltf", function(gltf) {
+    scene.add( gltf.scene );
+    //set scale and position of doll
+    gltf.scene.scale.set(.4, .4, .4);
+    gltf.scene.position.set(0, -1, 0);
+});
 
 // automatically animate geometry
 function animate() {
@@ -22,3 +37,12 @@ function animate() {
 	requestAnimationFrame( animate );
 }
 animate();
+
+// create mobile/responsive canvas
+window.addEventListener( 'resize' , onWindowResize, false );
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
